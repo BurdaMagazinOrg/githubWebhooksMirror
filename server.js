0af7186d80d69a7c9939943fe613adeed78cac00
Veronika Stürzer <veronika.stuerzer@burda.com>
2019-07-22 12:41:22 +0200

    updated jade to pug2

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.locals.pathprefix = settings.pathPrefix
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 app.use(settings.pathPrefix, express.static(path.resolve(__dirname, 'public')))
 
